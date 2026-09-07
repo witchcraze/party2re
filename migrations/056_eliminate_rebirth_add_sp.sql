7b13643706b5ebd53808220c6494467170e8cc7a
witchcraze <witchcraze@gmail.com>
2026-09-07 23:25:49 +0900

    fix(ranking): delete rebirth snapshot in migration and update integration test expectation (#470)

--- migrations/056_eliminate_rebirth_add_sp.sql
+++ migrations/056_eliminate_rebirth_add_sp.sql
@@ -5,5 +5,6 @@
 ALTER TABLE characters ADD COLUMN IF NOT EXISTS sp INT NOT NULL DEFAULT 0;
 DROP INDEX IF EXISTS idx_characters_rebirth ON characters;
 ALTER TABLE characters DROP COLUMN IF EXISTS rebirth_count;
+DELETE FROM ranking_snapshots WHERE ranking_type = 'rebirth';
 
 INSERT IGNORE INTO schema_migrations (version) VALUES ('056_eliminate_rebirth_add_sp');
